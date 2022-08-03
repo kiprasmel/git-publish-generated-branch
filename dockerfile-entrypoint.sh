57b30de2f564f25ea85048130bb866cd8e75a0d2
Kipras Melnikovas <kipras@kipras.org>
2022-08-03 07:26:00 +0300

    fix git's "dubious ownership" problem (because of github actions ofc)
    Signed-off-by: Kipras Melnikovas <kipras@kipras.org>

--- dockerfile-entrypoint.sh
+++ dockerfile-entrypoint.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
+# see https://github.com/webispy/checkpatch-action/commit/e605aacea0586dfb5e80e842aa453b00aed811f4
+git config --global --add safe.directory /github/workspace
+
 bash -c "/git-publish-generated-branch $*"
